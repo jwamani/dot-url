6bd0ccecbe8352ca8973871255ba5b8eadc48283
Wamani Joshua <joshuawamani100@gmail.com>
2026-06-26 16:36:08 +0300

    added interface for short link operations

---
+++ src/application/interfaces/link_repository.py
@@ -0,0 +1,31 @@
+"""absract classes for the short links"""
+
+from abc import ABC, abstractmethod
+
+from ...domain.entities.scode import ShortLink
+
+class IShortLinkRepository(ABC):
+    @abstractmethod
+    async def create_link(self, ShortLink) -> ShortLink:
+        """Create a new short link"""
+        pass
+    
+    @abstractmethod
+    async def get_by_code(self, code: str) -> ShortLink|None:
+        """Get a short link by its code"""
+        pass
+    
+    @abstractmethod
+    async def get_by_user(self, user_id: int) -> list[ShortLink]:
+        """Get all short links for a user"""
+        pass
+    
+    @abstractmethod
+    async def delete_link(self, code: str) -> None:
+        """Delete a short link by its code"""
+        pass
+    
+    @abstractmethod
+    async def increment_clicks(self, code: str) -> None:
+        """Increment the click count for a short link"""
+        pass
