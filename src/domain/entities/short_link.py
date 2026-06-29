4775ec4eae74a870abcea59f7c40fb4bf9b9118f
Wamani Joshua <joshuawamani100@gmail.com>
2026-06-29 13:59:23 +0300

    added proper database args

--- src/domain/entities/short_link.py
+++ src/domain/entities/short_link.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone as tz
 """id,code,original_url,created_at,click_count"""
 
 class ShortLink:
-    def __init__(self,id: int, user_id: int, scode: str, original_url: str, click_count: int, created_at: datetime|None, expires_at: datetime|None = None) -> None:
+    def __init__(self, user_id: int, scode: str, original_url: str, click_count: int, created_at: datetime|None, expires_at: datetime|None = None,id: int|None = None) -> None:
         self.id = id
         self.user_id = user_id
         self.short_code = scode
--- src/infrastructure/database/connection.py
+++ src/infrastructure/database/connection.py
@@ -1,9 +1,9 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncEngine
 
 
 def create_engine(db_url: str):
-    return create_async_engine(db_url)
+    return create_async_engine(db_url, pool_pre_ping=True, pool_recycle=3600)
 
 
-def create_session_factory(engine):
+def create_session_factory(engine: AsyncEngine):
     return async_sessionmaker(engine, expire_on_commit=False)
