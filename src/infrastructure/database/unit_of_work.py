4d30a8b69a92f3df3b29d8cdf18556cbe432811b
Wamani Joshua <joshuawamani100@gmail.com>
2026-07-01 12:47:52 +0300

    fixed missing session maker variable on content manager entry

--- src/infrastructure/database/unit_of_work.py
+++ src/infrastructure/database/unit_of_work.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from src.application.interfaces.unit_of_work import AbstractUnitOfWork
 from src.infrastructure.repositories.link_repository import ShortLinkRepository
@@ -6,15 +6,19 @@ from src.infrastructure.repositories.user_repository import UserRepository
 
 
 class UnitOfWork(AbstractUnitOfWork):
-    def __init__(self, session_factory: AsyncSession) -> None:
-        self._session = session_factory
+    def __init__(self, session_factory: async_sessionmaker) -> None:
+        self._session: AsyncSession = session_factory()
 
     async def __aenter__(self) -> AbstractUnitOfWork:
         self.links = ShortLinkRepository(self._session)
         self.users = UserRepository(self._session)
         return self
 
-    async def __aexit__(self, *args) -> None:
+    async def __aexit__(self,exc_type, *args) -> None:
+        if exc_type is not None:
+            await self.rollback()
+        else: 
+            await self.commit()
         await self._session.close()
 
     async def commit(self) -> None:
