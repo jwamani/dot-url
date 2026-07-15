832169acca77c0ab74bcdfb2a581a33090e9afbd
Wamani Joshua <joshuawamani100@gmail.com>
2026-07-15 17:46:57 +0300

    added validation for environment key

--- src/infrastructure/config.py
+++ src/infrastructure/config.py
@@ -1,10 +1,9 @@
 from pydantic_settings import BaseSettings
-
+from typing import Literal
 
 class Settings(BaseSettings):
     db_url: str
     redis_url: str = "redis://localhost:6379/0"
-    environment: str = "development"
+    environment: Literal["development", "production"]  = "development"
 
     model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
-
