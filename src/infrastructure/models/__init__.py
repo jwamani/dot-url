1ce33b89d7176628902404d8bdb44a5efadceed5
Wamani Joshua <joshuawamani100@gmail.com>
2026-07-15 19:30:50 +0300

    added missing inject in route

--- src/infrastructure/models/__init__.py
+++ src/infrastructure/models/__init__.py
@@ -1,4 +1,6 @@
 from src.infrastructure.models.user import User
 from src.infrastructure.models.short_link import ShortLink
+from src.infrastructure.models.link_count import LinkClick
+from src.infrastructure.database.base import Base
 
-__all__ = ["User", "ShortLink"]
+__all__ = ["User", "ShortLink", "LinkClick"]
--- src/presentation/api/v1/users.py
+++ src/presentation/api/v1/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from dependency_injector.wiring import Provide
+from dependency_injector.wiring import Provide, inject
 
 from src.application.use_cases import CreateUser
 from src.application.dtos.user_dtos import CreateUserRequest
@@ -9,5 +9,6 @@ from src.container import Container
 router = APIRouter(prefix="/users" ,tags=["Users"])
 
 @router.post("")
+@inject
 async def create_user(request: CreateUserRequest, usecase: CreateUser = Depends(Provide[Container.create_user])):
     return await usecase.execute(request)
